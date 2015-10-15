73e08845a7dfe504a8fed7fd804f5ce22933c2ee
Igor Kalkov <kalkov@embedded.rwth-aachen.de>
2015-10-15 11:08:58 +0200

    Added Android makefile for including into the source tree
    Change-Id: Ia6baa90d3d09b08553d95663edc670d104ae1073

---
+++ Android.mk
@@ -0,0 +1,14 @@
+# TODO: Add license information
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_SDK_VERSION := current
+
+LOCAL_PACKAGE_NAME := Benchmark
+
+include $(BUILD_PACKAGE)
